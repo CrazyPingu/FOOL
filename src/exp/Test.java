0d1acc950bfe688faa25adf823ad0e327926347b
Samuele De Tuglie <samuele.detuglie@studio.unibo.it>
2025-10-30 14:12:42 +0100

    Add negative number

---
+++ src/exp/Test.java
@@ -0,0 +1,31 @@
+package exp;
+
+import org.antlr.v4.runtime.CharStream;
+import org.antlr.v4.runtime.CharStreams;
+import org.antlr.v4.runtime.CommonTokenStream;
+import org.antlr.v4.runtime.tree.ParseTree;
+
+public class Test {
+    public static void main(String[] args) throws Exception {
+
+        String fileName = "prova.txt";
+
+        CharStream chars = CharStreams.fromFileName(fileName);
+        ExpLexer lexer = new ExpLexer(chars);
+        CommonTokenStream tokens = new CommonTokenStream(lexer);
+        ExpParser parser = new ExpParser(tokens);
+
+        ParseTree prog = parser.prog();
+
+        System.out.println("You had: " + lexer.lexicalErrors + " lexical errors and " +
+                parser.getNumberOfSyntaxErrors() + " syntax errors");
+
+        if (lexer.lexicalErrors + parser.getNumberOfSyntaxErrors() > 0) System.exit(1);
+
+        System.out.println("Calculating expression");
+
+        CalcSTVisitor visitor = new CalcSTVisitor();
+        System.out.println("The result is: " + visitor.visit(prog));
+
+    }
+}
